499928ca98b5c04f0e796f5cba781a1cea907b3c
Rohit <rohitbhagatkar940@gmail.com>
2025-04-07 16:36:30 +0100

    one more observable in Observer design pattern

--- Low Level Design/ObserverDesignPattern/Store.java
+++ Low Level Design/ObserverDesignPattern/Store.java
@@ -1,4 +1,5 @@
 import Observable.IphoneObservable;
+import Observable.SamsungObservable;
 import Observable.StocksObservable;
 import Observer.EmailAlertObserver;
 import Observer.MobileAlertObserver;
@@ -9,15 +10,17 @@ public class Store {
     public static void main(String[] args) {
 
         StocksObservable iphoneStockObservable = new IphoneObservable();
-
+        StocksObservable samsungStockObservable = new SamsungObservable();
         NotificationAlertObserver observer1 = new EmailAlertObserver("rohitbhagatkar940@gmail.com", iphoneStockObservable);
         NotificationAlertObserver observer2 = new EmailAlertObserver("np24437@bristol.ac.uk", iphoneStockObservable);
         NotificationAlertObserver observer3 = new MobileAlertObserver("Assassin", iphoneStockObservable);
+        NotificationAlertObserver observer4 = new EmailAlertObserver("rohitbhagatkar940@outlook.com",samsungStockObservable );
 
         iphoneStockObservable.add(observer1);
         iphoneStockObservable.add(observer2);
         iphoneStockObservable.add(observer3);
-
+        samsungStockObservable.add(observer4);
+        samsungStockObservable.setStockCount(1);
         iphoneStockObservable.setStockCount(10);
     }
 }
